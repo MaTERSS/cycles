93cd55051ef0a41131ae9dd9b48fa258ce6d112d
Ardor <Ardor@DESKTOP-BQF7240>
2024-09-06 17:46:18 +0800

    Добавьте файлы проекта.

---
+++ cycles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace CSharplight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            int quantity;
+            Console.WriteLine("Введите число,сколько раз будет выведена фраза:");
+            quantity = Convert.ToInt32(Console.ReadLine());
+            while (quantity-- > 0)
+            {
+                Console.WriteLine("Кратос бегает по горам");
+            }
+        }
+    }
+}
